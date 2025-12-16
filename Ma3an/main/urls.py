ea555bbf572f223c13caf3676903eacd8a8f2b75
Norah <norahwasr@gmail.com>
2025-12-16 15:01:49 +0300

    initial structure

---
+++ Ma3an/main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = "main"
+
+urlpatterns = [
+    path("", views.home_view, name="home_view"),
+
+]
